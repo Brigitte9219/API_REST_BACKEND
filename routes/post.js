181bbcbef2414fc13b690bc0349585a9ef13cee7
Brigitte Padilla <mbpadilla4@misena.edu.co>
2023-10-15 16:41:04 -0500

    Configuración de las rutas

---
+++ routes/post.js
@@ -0,0 +1,54 @@
+router.post('/', async(req,res) =>{
+
+    //console.log(req.body); Se utiliza para la respuesta del post en consola
+    const post = new Post ({
+        title: req.body.title,
+        description: req.body.description
+    });
+    try{
+        const savedPost = await post.save();//método que guarda en la BD
+        res.json(savedPost);
+    }catch (error){
+        res.json({message: error});
+    }
+});
+
+/**
+ * Bloque para mostrar solo un post por el Id
+ */
+router.get('/:postId', async (req,res)=>{
+    try{
+        const post = await post.findById(req.params.postId);//encuentra por id
+        res.json(post);
+    }catch (error){
+        res.json({message: error});
+    }
+});
+
+/**
+ * Bloque para borrar un post
+ */
+router.delete('/:postId', async (req,res)=>{
+    try{
+        const removedPost= await post.remove({_id: res.params.postId});//borra
+        res.json(removedPost);
+    }catch (error){
+        res.json({message: error});
+    }
+});
+
+/**
+ * Actualizar un post
+ */
+router.patch('/:postId', async(req,res)=> {//patch para actualizar
+    try{
+        const updatePost= await Post.updateOne(//actualiza de uno en uno
+        {_id: req.params.postId},
+        {$set: {title: req.body.title}});
+        res.json(updatePost);
+    }catch (error){
+        res.json({message: error});
+    }
+});
+
+module.exports = router; //devuelve como módulo lo que se le asigna a route
