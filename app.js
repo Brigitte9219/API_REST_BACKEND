fc357171df9661a9cbeba031266ea8c90545394b
Brigitte Padilla <mbpadilla4@misena.edu.co>
2023-10-15 15:10:56 -0500

    Creación del archivo app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+//Iniciamos el módulo express con el fin de dar inicio al servidor, evitando varias configuraciones
+const express = require('express');//importamos el paquete
+const app = express();
+
+/* SE CREAN LAS RUTAS */
+app.get('/',(req, res) =>{
+    res.send('prueba 1 respuesta del servidor');//Ruta por defecto
+});
+//Primero se configura cómo va escuchar el servidor las peticiones
+app.listen(10000);
