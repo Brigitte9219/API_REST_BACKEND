0fc59c67fe4e35532824e0bce77610bce04284c3
Brigitte Padilla <mbpadilla4@misena.edu.co>
2023-10-15 15:32:12 -0500

    Conexion BD

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 //Iniciamos el módulo express con el fin de dar inicio al servidor, evitando varias configuraciones
 const express = require('express');//importamos el paquete
 const app = express();
+const mongoose = require('mongoose');//se utiliza para hacer la conexión a la BD
 
 /* SE CREAN LAS RUTAS */
 app.get('/',(req, res) =>{
     res.send('prueba 1 respuesta del servidor');//Ruta por defecto
+});
+
+//Conexión a la BD
+mongoose.connect('mongodb+srv://mbpadilla4:<password>@cluster0.hiktsv1.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true}, () =>{
+    console.log('Si hay conexión a la BD');
 });
 //Primero se configura cómo va escuchar el servidor las peticiones
 app.listen(10000);
