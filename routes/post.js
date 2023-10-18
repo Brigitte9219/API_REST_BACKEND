5da086668f827fe21f06cff9bf3fdc8c45597f29
Brigitte Padilla <mbpadilla4@misena.edu.co>
2023-10-18 16:16:40 -0500

    Actualización de post.js - definir variable router

--- routes/post.js
+++ routes/post.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const router =express.Router();
+
 router.post('/', async (req,res) =>{
 
     //console.log(req.body); Se utiliza para la respuesta del post en consola
